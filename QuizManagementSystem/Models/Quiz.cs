f15ed68f5c50efda5a687c7c4be8befeb1eeb306
cristopherUwU <cristophermir08@gmail.com>
2024-10-03 17:44:36 -0400

    feat: create constructor for quiz class

--- QuizManagementSystem/Models/Quiz.cs
+++ QuizManagementSystem/Models/Quiz.cs
@@ -7,5 +7,12 @@ namespace QuizManagementSystem.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public List<Question> Questions { get; set; } = new List<Question>();
+        
+        public Quiz(string title, string description, List<Question> questions)
+        {
+            Title = title;
+            Description = description;
+            Questions = questions;
+        }
     }
 }
